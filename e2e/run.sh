3422b4f228cd0db8378eaee1e6bf1bd5d95ee164
Avinash Rai <33398974+Avnshrai@users.noreply.github.com>
2024-05-08 10:46:30 +0530

    Update run.sh

--- e2e/run.sh
+++ e2e/run.sh
@@ -8,7 +8,7 @@ IFS=$'\n\t'
 
 readonly cluster_name="postgres-operator-e2e-tests"
 readonly kubeconfig_path="/tmp/kind-config-${cluster_name}"
-readonly spilo_image="coredgeio/postgres-spilo:3.0-p1"
+readonly spilo_image="registry.opensource.zalan.do/acid/spilo-16-e2e:0.1"
 readonly e2e_test_runner_image="registry.opensource.zalan.do/acid/postgres-operator-e2e-tests-runner:0.4"
 
 export GOPATH=${GOPATH-~/go}
@@ -18,14 +18,12 @@ echo "Clustername: ${cluster_name}"
 echo "Kubeconfig path: ${kubeconfig_path}"
 
 function pull_images(){
-  echo "pulling image"
-  local operator_tag="$1"
-  if [[ -z $(coredgeio/postgres-operator:${operator_tag}) ]]
+  operator_tag=$(git describe --tags --always --dirty)
+  if [[ -z $(docker images -q registry.opensource.zalan.do/acid/postgres-operator:${operator_tag}) ]]
   then
-    docker pull coredgeio/postgres-operator:${operator_tag}
+    docker pull registry.opensource.zalan.do/acid/postgres-operator:latest
   fi
-  operator_image="coredgeio/postgres-operator:${operator_tag}"
-  echo "pulling image done!"
+  operator_image=$(docker images --filter=reference="registry.opensource.zalan.do/acid/postgres-operator" --format "{{.Repository}}:{{.Tag}}" | head -1)
 }
 
 function start_kind(){
@@ -35,16 +33,11 @@ function start_kind(){
   then
     kind delete cluster --name ${cluster_name}
   fi
-  echo "export and create cluster"
+
   export KUBECONFIG="${kubeconfig_path}"
-  kind create cluster --name ${cluster_name} --config e2e/kind-cluster-postgres-operator-e2e-tests.yaml  
-  echo "export and create cluster done!"
-  echo "pulling spilo image"
+  kind create cluster --name ${cluster_name} --config kind-cluster-postgres-operator-e2e-tests.yaml  
   docker pull "${spilo_image}"
-  echo "done pulling spilo image"
-  echo "kind load"
   kind load docker-image "${spilo_image}" --name ${cluster_name}
-  echo "kind load done"
 }
 
 function load_operator_image() {
@@ -73,9 +66,10 @@ function run_tests(){
   docker run --rm --network=host -e "TERM=xterm-256color" \
   --mount type=bind,source="$(readlink -f ${kubeconfig_path})",target=/root/.kube/config \
   --mount type=bind,source="$(readlink -f manifests)",target=/manifests \
-  --mount type=bind,source="$(readlink -f e2e/tests)",target=/tests \
-  --mount type=bind,source="$(readlink -f e2e/exec.sh)",target=/exec.sh \
-  --mount type=bind,source="$(readlink -f e2e/scripts)",target=/scripts \
+  --mount type=bind,source="$(readlink -f tls)",target=/tls \
+  --mount type=bind,source="$(readlink -f tests)",target=/tests \
+  --mount type=bind,source="$(readlink -f exec.sh)",target=/exec.sh \
+  --mount type=bind,source="$(readlink -f scripts)",target=/scripts \
   -e OPERATOR_IMAGE="${operator_image}" "${e2e_test_runner_image}" ${E2E_TEST_CASE-} $@
 }
 
@@ -89,17 +83,15 @@ function cleanup(){
 function main(){
   echo "Entering main function..."
   [[ -z ${NOCLEANUP-} ]] && trap "cleanup" QUIT TERM EXIT
-  local operator_tag="$1"
-  pull_images "${operator_tag}"
+  pull_images
   [[ ! -f ${kubeconfig_path} ]] && start_kind
   load_operator_image
   set_kind_api_server_ip
-  # generate_certificate
+  generate_certificate
 
   shift
   run_tests $@
   exit 0
 }
 
-main "$@"
-
+"$1" $@
